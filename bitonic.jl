5b641e0a61ca091e4bf7e3f652ddade8ac8a2f54
Max Ferguson <maxkferg@gmail.com>
2015-04-15 18:37:45 -0700

    Bitonic sorting working with 1 value per processor

--- bitonic.jl
+++ bitonic.jl
@@ -10,9 +10,8 @@ println("$rank: Starting up")
 
 
 function bitonic_sort(x,up,ranks)
-    println("$x")
     if length(ranks) <= 1
-        return x
+        return ranks
     else
         dist = round(Int,length(ranks) / 2)
         first = bitonic_sort(x, true, ranks[1:dist])
@@ -43,10 +42,18 @@ function bitonic_compare(x,up,ranks)
     # Perform a bitonic compare between the processors in @ranks
     dist = round(Int,length(ranks) / 2)
     for i in 1:dist
-        if (ranks[i] > ranks[i + dist])==up
-            ranks[i], ranks[i + dist] = ranks[i + dist], ranks[i] #swap
+        if rank==ranks[i]
+            next = rank + dist;
+            println("Comparing rank=$rank -> rank=$next")
+            #if (ranks[i] > ranks[i + dist])==up
+            #    ranks[i], ranks[i + dist] = ranks[i + dist], ranks[i] #swap
+            #end
         end
-        #bitonic_swap(x,up,ranks[i],ranks[i + dist])
+        bitonic_swap(x,up,ranks[i],ranks[i + dist])
+        if rank==ranks[i]
+            #MPI.Barrier(comm)
+            #gather(x)
+        end
     end
 end
 
@@ -62,13 +69,13 @@ function bitonic_swap(x,up,ranka,rankb)
  
     # Send the data over to processor rank+1
     if (rank==ranka)
-        println("swap $ranka to $rankb :: up = $up")
+        #println("swap $ranka to $rankb :: up = $up")
     
         #println("$rank: Sending $rank -> $rankb = $x ")
         MPI.send(x, rankb, rank+32, comm)
         #println("$rank: Waiting for message from $rankb")
         newx = MPI.recv(rankb, MPI.ANY_TAG, comm)[1]
-        println("$rank: Receiving $ranka -> $rank = $newx ")
+        #println("$rank: Receiving $ranka -> $rank = $newx ")
 
     elseif (rank==rankb)   
         x_src = MPI.recv(ranka,  ranka+32, comm)[1]
@@ -79,94 +86,17 @@ function bitonic_swap(x,up,ranka,rankb)
         newx = x_merged[dist+1:end]
         s = x_merged[1:dist]
         MPI.send(s, ranka, rank+128, comm)
-        println("$rank: Bouncing $rank -> $ranka = $x ") 
+        #println("$rank: Bouncing $rank -> $ranka = $x ") 
     end
 
     # Modify x in place with newx
-    for i in 1:length(newx)
-        x[i] = newx[i]
-    end
+    #for i in 1:length(newx)-1
+    x[1] = newx[1]
+    #end
 
     sort!(x, rev=~up);
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function bitonic_compare_old(x,min,max)
-    # Form a bitonic vector that stretches from min->max
-    # @min and @max are processor ranks
-    # Min is included in the bitonic vector but max is not
-    midpoint = round(Int, abs(min-max)/2)
-    shift = round(Int, abs(min-max)/2)
-
-    if shift<1
-        return x
-    end 
-
-    x = bitonic_compare(x,min,midpoint)
-    #x = bitonic_compare(x,midpoint+1,max)
-    println("We should now have bitonic $min->$midpoint + $midpoint->$max")
-    gather(x)
-
-    # Make our bigger bitonic sequence
-    for processor in min:midpoint-1
-        println("Swapping $processor -> $processor+$shift")
-        x = bitonic_swap(x,true,processor,processor+shift)
-        gather(x)
-
-    end 
-    return x
-    #for processor in midpoint:max-shift
-    #    x = bitonic_compare(x,processor,max-shift)
-    #    println("bitonic $min to $max :: shift = $shift, mid=$midpoint")
-    #    #x=bitonic_swap(x,false,processor,processor+shift)
-    #    #x = bitonic_compare(x,processor,processor+shift)
-    #end
-    #return x
-        #println(x)
-        #x=bitonic_swap(x,true,1,1+shift)
-        #println("$x")
-
-        #x=bitonic_swap(x,true,2,2+shift)
-        #println(x)
-        #x=bitonic_swap(x,false,3,3+shift)
-        #println(x)
-        #x=bitonic_swap(x,false,3,4+shift) 
-        #println(x)    
-        # x=bitonic_swap(x,false,3,5+shift)
-        #println(x)
-        #end
-    #end
-
-
-    #x=bitonic_swap(x,true,0,3)
-    #x=bitonic_swap(x,true,1,3)
-    #x=bitonic_swap(x,true,2,3)
-    #up = rank<size/2
-    #x = sort(x,rev=~up)
-    
-    # Now perform bitonic merge
-    #x=bitonic_swap(x,true,0,2)
-    #x=bitonic_swap(x,true,1,3)
-
-    #x=bitonic_swap(x,true,0,1)
-    #x=bitonic_swap(x,true,2,3)
-
-end
 
 
 
@@ -205,23 +135,19 @@ end
 
 
 N = 2
-#x = vec(rand(1:100,1,N))
-
-
-x = vec([18-rank]);
-x=bitonic_sort(x,true,[0,1,2,3,4,5,6,7])
-println("xxx=$x")
-
-
-
-
+x = vec(rand(1:100,1,1))
+ranks = bitonic_sort(x,true,[0,1,2,3,4,5,6,7])
+println("RANKS=$ranks")
 
 println("[$rank] done")
-MPI.Barrier(comm)
-
 gather(x)
-
 MPI.Barrier(comm)
+
+
+
+
+
+
 
 
 
